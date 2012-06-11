3e806ce60316fd3adba323f733786f8157d68806
jpayne <jpayne@696648.no-reply.drupal.org>
2012-06-11 10:32:21 -0400

    Issue #1491264 by acrazyanimal:Fixed notice in template file

--- entityform.tpl.php
+++ entityform.tpl.php
@@ -31,7 +31,11 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if (!$page): ?>
     <h2<?php print $title_attributes; ?>>
+      <?php if (!empty($url)): ?>
         <a href="<?php print $url; ?>"><?php print $title; ?></a>
+      <?php else: ?>
+        <?php print $title; ?>
+      <?php endif; ?>
     </h2>
   <?php endif; ?>
 
